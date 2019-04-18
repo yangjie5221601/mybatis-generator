547aa85c53839b4a57384b6a53fba0536cc91379
yangjie <yangjie@benmu-health.com>
2019-04-18 18:52:43 +0800

    11

--- src/main/java/com/mybatis/generator/MybatisGenerator.java
+++ src/main/java/com/mybatis/generator/MybatisGenerator.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class MybatisGenerator {
     public static void main(String[] args) throws URISyntaxException {
         try {
-            //1
+            //112
             List<String> warnings = new ArrayList<String>();
             boolean overwrite = true;
             ClassLoader classloader = Thread.currentThread().getContextClassLoader();
